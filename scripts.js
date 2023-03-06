d2fcc3c886f11caf13ad1fcba818962381ae239b
ZF5657 <zane56fields57@gmail.com>
2023-03-05 22:14:24 -0500

    Add container and button
    Added div container and button

--- scripts.js
+++ scripts.js
@@ -1,8 +1,24 @@
 const mainContainer = document.querySelector('#mainContainer');
+const divContainer = document.querySelector('#divContainer');
+
+divContainer.style.border = "none";
+divContainer.style = "margin-top: 50px";
+divContainer.style = "margin-bottom: 1px";
+
+
+const gridButton = document.createElement('button');
+mainContainer.insertBefore(gridButton, divContainer);
+gridButton.style = "margin-top: 20px";
+gridButton.style = "margin-bottom: 10px";
+gridButton.style.height = "50px";
+gridButton.style.width = "100px";
+
+
+//creates div boxes within divContainer
 for(i=0; i<256; i++) {
 const div = document.createElement('div');
 div.style = "flex: 1 1 6%";
-div.style.height = "70px";
+div.style.height = "60px";
 div.style.border = "1px solid black";
-mainContainer.appendChild(div);
+divContainer.appendChild(div);
 }
