ba692f1e093bbf930510def9544009f094b568c5
ZF5657 <zane56fields57@gmail.com>
2023-03-19 16:11:22 -0400

    Add draw function
    Will need to fix and add functionality

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const mainContainer = document.querySelector('#mainContainer');
 const divContainer = document.querySelector('#divContainer');
 
+
 divContainer.style.border = "none";
 divContainer.setAttribute('style', 'margin-top: 10px');
 
@@ -15,11 +16,36 @@ gridButton.style.color = 'blue';
 gridButton.textContent = 'ADJUST GRID';
 
 
+
 //creates div boxes within divContainer
 for(i=0; i<256; i++) {
 const div = document.createElement('div');
 div.style = "flex: 1 1 6%";
 div.style.height = "60px";
 div.style.border = "1px solid black";
+div.setAttribute =('class', 'canvas');
 divContainer.appendChild(div);
 }
+
+
+const canvas = document.querySelectorAll('.canvas');
+
+
+
+
+canvas.forEach(div => {
+    canvas.addEventListener("mousemove", draw)
+})
+
+
+
+
+function draw (e) {
+    canvasLine.beginPath();
+    canvasLine.lineWidth = '2';
+    canvasLine.strokeStyle = 'black';
+    canvasLine.stroke();
+}
+
+
+
