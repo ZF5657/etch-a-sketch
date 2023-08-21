6b958cfc42de64bef9bf492c92997478e7f25246
ZF5657 <zane56fields57@gmail.com>
2023-08-21 18:10:18 -0400

    Add grid and buttons

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+/*document.addEventListener("DOMContentLoaded", function (){
+    createGrid(32);
+});*/
+
 const mainContainer = document.querySelector('.main-container');
 mainContainer.stlye = ('display: flex; flex-wrap: wrap; justify-content: center; align-items: center;');
 
@@ -8,7 +12,7 @@ headerContainer.style = ('margin-top: 20px')
 
 const headerP = document.createElement("p");
 headerContainer.appendChild(headerP);
-headerP.textContent = 'Choose how many squares you would like in your canvas (10 = 10x10 to maximum 100 = 100x100)';
+headerP.textContent = 'Choose how many squares you would like in your canvas (1 = 1x1 to maximum 100 = 100x100)';
 headerP.style = ('margin-bottom: 10px');
 
 
@@ -25,32 +29,66 @@ inputButton.style = ('padding: 3px');
 headerContainer.appendChild(inputButton);
 
 
-const canvasContainer = document.querySelector('.canvas-container');
+let canvasContainer = document.querySelector('.canvas-container');
 canvasContainer.style = ('height: 600px; width: 600px; display: grid; border: 1px solid black; margin-top: 20px;');
+
 mainContainer.appendChild(canvasContainer);
 
 
 const footerContainer = document.querySelector('.footer');
+footerContainer.style = ('display: flex; justify-content: space-between; margin-top: 20px')
 mainContainer.appendChild(footerContainer);
+
+
+const clearButton = document.createElement('button');
+clearButton.textContent = 'Clear';
+clearButton.setAttribute('class', 'btn btn-dark')
+clearButton.style = ('padding: 3px');
+footerContainer.appendChild(clearButton);
+
 
 const resetButton = document.createElement('button');
 resetButton.textContent = 'Reset';
 resetButton.setAttribute('class', 'btn btn-warning')
-resetButton.style = ('margin-top: 20px; padding: 3px');
+resetButton.style = ('padding: 3px');
 footerContainer.appendChild(resetButton);
 
-/*for(i=0; i<3; i++) {
-    const div = document.createElement('div');
-    div.style = ('display: flex', 'flex: 1 1 auto', 'border: 1px solid black');
-    div.setAttribute =('class', 'canvas');
-    mainContainer.appendChild(div);
-    }
 
-let sketchPad = (div) => {
-    div = document.createElement("div")
-    div.setAttribute()
-    mainContainer.appendChild
+const randomColorButton = document.createElement('button');
+randomColorButton.textContent = 'Random Color';
+randomColorButton.setAttribute('class', 'btn btn-primary')
+randomColorButton.style = ('padding: 3px;');
+footerContainer.appendChild(randomColorButton);
+
+
+/*const createGrid = (num) => {
+    canvasContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`
+    canvasContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`
+
+    let numDivs = num * num;
+
+    for(let i = 0; i < numDivs; i++) {
+        let gridDiv = document.createElement('div');
+        canvasContainer.insertAdjacentElement("beforeend", div);
+
+    };
 }*/
 
+
+const createGrid = (num) => {
+    canvasContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`
+    canvasContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`
+
+    let numDivs = num * num;
+
+    for(let i = 0; i < numDivs; i++) {
+        let gridDiv = document.createElement('div');
+        canvasContainer.appendChild(gridDiv);
+
+    };
+}
+
+
+createGrid(32);
 
 
