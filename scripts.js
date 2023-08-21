54598b9fc9ca81560f2bd1a3b1c1bbe14df8942b
ZF5657 <zane56fields57@gmail.com>
2023-08-21 16:18:13 -0400

    Adjust styling and script

--- scripts.js
+++ scripts.js
@@ -1,51 +1,56 @@
-const mainContainer = document.querySelector('#mainContainer');
-const divContainer = document.querySelector('#divContainer');
+const mainContainer = document.querySelector('.main-container');
+mainContainer.stlye = ('display: flex; flex-wrap: wrap; justify-content: center; align-items: center;');
+
+const headerContainer = document.querySelector('.header');
+mainContainer.appendChild(headerContainer);
+headerContainer.style = ('margin-top: 20px')
 
 
-divContainer.style.border = "none";
-divContainer.setAttribute('style', 'margin-top: 10px');
+const headerP = document.createElement("p");
+headerContainer.appendChild(headerP);
+headerP.textContent = 'Choose how many squares you would like in your canvas (10 = 10x10 to maximum 100 = 100x100)';
+headerP.style = ('margin-bottom: 10px');
 
 
-
-const gridButton = document.createElement('button');
-mainContainer.insertBefore(gridButton, divContainer);
-gridButton.setAttribute('style', 'margin-top: 10px; margin-bottom: 10px; background-color: rgb(300, 200, 140); font-weight: bold');
-gridButton.style.height = "30px";
-gridButton.style.width = "150px";
-gridButton.style.color = 'blue';
-gridButton.textContent = 'ADJUST GRID';
+const gridInput = document.createElement("input");
+gridInput.setAttribute('type', 'text');
+gridInput.style = ('width: 35px; margin-right: 20px');
+headerContainer.appendChild(gridInput);
 
 
-
-//creates div boxes within divContainer
-for(i=0; i<256; i++) {
-const div = document.createElement('div');
-div.style = "flex: 1 1 6%";
-div.style.height = "60px";
-div.style.border = "1px solid black";
-div.setAttribute =('class', 'canvas');
-divContainer.appendChild(div);
-}
+const inputButton = document.createElement("button");
+inputButton.textContent = 'Submit';
+inputButton.setAttribute('class', 'btn btn-info');
+inputButton.style = ('padding: 3px');
+headerContainer.appendChild(inputButton);
 
 
-const canvas = document.querySelectorAll('.canvas');
+const canvasContainer = document.querySelector('.canvas-container');
+canvasContainer.style = ('height: 600px; width: 600px; display: grid; border: 1px solid black; margin-top: 20px;');
+mainContainer.appendChild(canvasContainer);
 
 
+const footerContainer = document.querySelector('.footer');
+mainContainer.appendChild(footerContainer);
 
+const resetButton = document.createElement('button');
+resetButton.textContent = 'Reset';
+resetButton.setAttribute('class', 'btn btn-warning')
+resetButton.style = ('margin-top: 20px; padding: 3px');
+footerContainer.appendChild(resetButton);
 
-canvas.forEach(div => {
-    canvas.addEventListener("mousemove", draw)
-})
+/*for(i=0; i<3; i++) {
+    const div = document.createElement('div');
+    div.style = ('display: flex', 'flex: 1 1 auto', 'border: 1px solid black');
+    div.setAttribute =('class', 'canvas');
+    mainContainer.appendChild(div);
+    }
 
-
-
-
-function draw (e) {
-    canvasLine.beginPath();
-    canvasLine.lineWidth = '2';
-    canvasLine.strokeStyle = 'black';
-    canvasLine.stroke();
-}
+let sketchPad = (div) => {
+    div = document.createElement("div")
+    div.setAttribute()
+    mainContainer.appendChild
+}*/
 
 
 
